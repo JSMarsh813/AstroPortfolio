e1e3a7e21af526a99d256d3972c39329ca3f7084
JSMarsh813 <janetspellman13@gmail.com>
2024-01-23 00:35:13 -0800

    added blank alt tags to decorative images, added sitemap, added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://main--vermillion-mandazi-4ce414.netlify.app/",
+  integrations: [react(), tailwind(), sitemap()],
   output: "hybrid",
   adapter: netlify(),
 });
--- src/components/landingpage/DiamondBackgroundImages.jsx
+++ src/components/landingpage/DiamondBackgroundImages.jsx
@@ -6,7 +6,8 @@ export default function DiamondBackgroundImages({ imgSrc }) {
       {/* image needs to be in div, so the image centers in the y axis correctly when the div rotated into a diamond shape */}
       <img
         src={`/images/${imgSrc}.svg`}
-        class="min-w-24 p-4 shadow-transition-effect "
+        className="min-w-24 p-4 shadow-transition-effect"
+        alt=""
       />
     </div>
   );
