42484eb7c863736b284c7356e03f7f7176c38503
JSMarsh813 <janetspellman13@gmail.com>
2024-01-23 01:52:06 -0800

    fixed typo in inline background image, removed unneeded import from index.astro

---
+++ .netlify/functions-internal/ssr/chunks/generic_6yA-pItl.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_H_MITd1U.mjs');
+
+export { page };
