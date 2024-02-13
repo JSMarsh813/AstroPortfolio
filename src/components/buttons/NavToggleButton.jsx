55b449e1f0887db8d0fffb5b887eb8caef67c33e
JSMarsh813 <janetspellman13@gmail.com>
2024-02-13 00:52:44 -0800

    removed store and unnecessary code from simplied mobile nav, removed unused code, fixed nav button svg

--- src/components/buttons/NavToggleButton.jsx
+++ src/components/buttons/NavToggleButton.jsx
@@ -1,63 +1,22 @@
 import { useState } from "react";
-import { useEffect } from "react";
-import { useStore } from "@nanostores/react";
-import { isOpen } from "../../store";
-import { userClicked } from "../../store";
-import { windowWidth } from "../../store";
 
 export default function NavToggleButton() {
   const [iconMenuClose, setIconMenuClose] = useState("icon-menu-close");
   const [hamburgerImage, setHamburgerImage] = useState("");
-  let open = useStore(isOpen);
-  let userClickedStatus = useStore(userClicked);
-
-  const [windowSize, setWindowSize] = useState([
-    window.innerWidth,
-    window.innerHeight,
-  ]);
-
-  useEffect(() => {
-    const windowSizeHandler = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-      windowWidth.set(window.innerWidth);
-    };
-    window.addEventListener("resize", windowSizeHandler);
-
-    return () => {
-      window.removeEventListener("resize", windowSizeHandler);
-    };
-  }, []);
-
-  //   have navs automatically close below 800px, if user has not clicked the hamburger button
-  useEffect(() => {
-    if (userClickedStatus == false) {
-      let resizeSidebarState = windowSize[0] > 800 ? true : false;
-      isOpen.set(resizeSidebarState);
-    } else {
-      return;
-    }
-  }, [windowSize]);
+  const [openNav, setOpenNav] = useState(false);
 
   return (
-    <div className={`bg-mainColor py-2 md:hidden`}>
-      {open ? (
+    <div className={`bg-mainColor md:hidden flex justify-center`}>
+      {openNav ? (
         <button
           onClick={() => {
-            isOpen.set(false);
-            userClicked.set(true);
+            setOpenNav(false);
           }}
           aria-expanded={open}
           aria-controls="sidebar"
           aria-label="close Menu"
-          className="text-shadow-lg text-shadow-white pl-2 mt-2"
+          className="pl-2"
         >
-          <a
-            className="skip-link right-5 top-5 p-3"
-            href="#social"
-          >
-            Skip to social links
-          </a>
-
           <img
             className=""
             src={`/images/${iconMenuClose}.svg`}
@@ -69,8 +28,7 @@ export default function NavToggleButton() {
       ) : (
         <button
           onClick={() => {
-            isOpen.set(true);
-            userClicked.set(true);
+            setOpenNav(true);
           }}
           aria-expanded={open}
           aria-controls="sidebar"
--- src/components/navigation/MobileNav.jsx
+++ src/components/navigation/MobileNav.jsx
@@ -1,95 +1,76 @@
-import { useState } from "react";
-import { Disclosure, Transition } from "@headlessui/react";
-import { useStore } from "@nanostores/react";
-import { isOpen } from "../../store";
-export default function MobileNav() {
-  let open = useStore(isOpen);
-  console.log(open);
+import { Disclosure } from "@headlessui/react";
 
+export default function MobileNav() {
   return (
     <>
-      <Transition show={open}>
-        {/* Sliding sidebar */}
+      {/*  ********************    MAIN NAV, DOTTED BACKGROUND*****************  */}
+      <div
+        id="sidebar"
+        className={` w-screen bg-mainColor gradient-line border-t-4`}
+        style={{ backgroundImage: `url("/images/dotsBackground.png")` }}
+      >
+        <ul className="flex flex-col space-y-3 pt-6 bg-mainColor text-center">
+          {/* ************************** FIRST ITEM ***********************  */}
+          <li className="glow gradient-line border-b-2 pb-1">
+            <a href="/#home">
+              <span className="text-center">HOME</span>
+            </a>
+          </li>
 
-        <Transition.Child
-          className="left-0 top-0 w-screen"
-          enter="transition ease-in-out duration-600 transform"
-          enterFrom="translate-x-0"
-          enterTo="translate-x-0"
-          leave="transition ease-in-out duration-600 transform"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-full"
-        >
-          {/*  ********************    MAIN NAV, DOTTED BACKGROUND*****************  */}
-          <div
-            id="sidebar"
-            className={` w-screen bg-mainColor gradient-line border-t-4`}
-            style={{ backgroundImage: `url("/images/dotsBackground.png")` }}
-          >
-            <ul className="flex flex-col space-y-3 pt-6 bg-mainColor text-center">
-              {/* ************************** FIRST ITEM ***********************  */}
-              <li className="glow gradient-line border-b-2 pb-1">
-                <a href="/#home">
-                  <span className="text-center">HOME</span>
-                </a>
-              </li>
+          <li className="gradient-line border-b-2 pb-1">
+            {/* ************************** PROJECTS/ DROPPABLE LIST ***********************  */}
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex mx-auto ">
+                    <span className="pl-6 mr-2 glow ">PROJECTS </span>
+                    <img
+                      className=""
+                      src={`/images/icon-arrow-${open ? "up" : "down"}.svg`}
+                      alt="Arrow"
+                    />
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                    <ul className="flex flex-col space-y-3">
+                      <li className="cursor-pointer glow ">
+                        <a href="/#projects">NEWEST PROJECTS</a>
+                      </li>
 
-              <li className="gradient-line border-b-2 pb-1">
-                {/* ************************** PROJECTS/ DROPPABLE LIST ***********************  */}
-                <Disclosure>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="flex mx-auto ">
-                        <span className="pl-6 mr-2 glow ">PROJECTS </span>
-                        <img
-                          className=""
-                          src={`/images/icon-arrow-${open ? "up" : "down"}.svg`}
-                          alt="Arrow"
-                        />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
-                        <ul className="flex flex-col space-y-3">
-                          <li className="cursor-pointer glow ">
-                            <a href="/#projects">NEWEST PROJECTS</a>
-                          </li>
+                      <li className="cursor-pointer glow">
+                        <a href="/otherprojects/">OTHER PROJECTS</a>
+                      </li>
+                    </ul>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </li>
 
-                          <li className="cursor-pointer glow">
-                            <a href="/otherprojects/">OTHER PROJECTS</a>
-                          </li>
-                        </ul>
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
-              </li>
+          <li className="glow gradient-line border-b-2 pb-1">
+            <a href="/about/">ABOUT ME</a>
+          </li>
 
-              <li className="glow gradient-line border-b-2 pb-1">
-                <a href="/about/">ABOUT ME</a>
-              </li>
+          <li className="glow gradient-line border-b-2 pb-1">
+            <a href="/#services">
+              <span>SERVICES</span>
+            </a>
+          </li>
 
-              <li className="glow gradient-line border-b-2 pb-1">
-                <a href="/#services">
-                  <span>SERVICES</span>
-                </a>
-              </li>
+          <li className="glow gradient-line border-b-2 pb-1">
+            <a href="/#process">THE PROCESS</a>
+          </li>
 
-              <li className="glow gradient-line border-b-2 pb-1">
-                <a href="/#process">THE PROCESS</a>
-              </li>
+          <li className="glow gradient-line border-b-2 pb-1">
+            <a href="/#skills">SKILLS</a>
+          </li>
 
-              <li className="glow gradient-line border-b-2 pb-1">
-                <a href="/#skills">SKILLS</a>
-              </li>
+          <li className="glow gradient-line border-b-2 pb-1">
+            <a href="/#contact">CONTACT</a>
+          </li>
+        </ul>
 
-              <li className="glow gradient-line border-b-2 pb-1">
-                <a href="/#contact">CONTACT</a>
-              </li>
-            </ul>
-
-            <div className="flex flex-col space-y-5 items-center justify-items-center pt-8"></div>
-          </div>
-        </Transition.Child>
-      </Transition>
+        <div className="flex flex-col space-y-5 items-center justify-items-center pt-8"></div>
+      </div>
     </>
   );
 }
