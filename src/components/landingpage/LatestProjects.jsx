15d05900397f0e70004a5a0c0b1c49a6f62df62e
Janet Spellman <janetspellman13@gmail.com>
2025-08-24 11:50:34 -0700

    wording: added project languages to wipe right

--- src/components/landingpage/LatestProjects.jsx
+++ src/components/landingpage/LatestProjects.jsx
@@ -9,7 +9,7 @@ export default function LatestProjects() {
           vidTitles={"Video of Wipe Right"}
           projectTitle={"Wipe Right"}
           projectLanguages={
-            " Next.js 15, React 19, Appwrite, TypeScript, Tailwindcss, SWR, React-hook-form, React-Select, HeadlessUI,CSS, HTML5"
+            " Next.js 14, TypeScript, Tailwindcss, Supabase, Fast API, React Context, TanStack Query, Open AI, Stripe, HeroUi"
           }
           projectDescription={
             "Finally there's an app to easily connect with a service provider for everyday needs such as: plumbing, cleaning, lawn care and moving! Additionally users can use the speech-to-text feature to chat with Bumi AI to quickly make emergency appointments, get service suggestions, or cancel and reschedule existing appointments. Providers can easily update their clients about their status with a simple click, or text their client. Additionally we offer an interactive provider map so providers can easily see their bookings route. Created in 6 weeks with 6 team members as part of the Dallas Software Developers Cohort, voted best project among peers from 10 other teams."
@@ -47,7 +47,7 @@ export default function LatestProjects() {
           vidTitles={"Video of tailored Pet Names website"}
           projectTitle={"Tailored Pet Names"}
           projectLanguages={
-            " Next.js, React, Next-Auth, JavaScript, Tailwindcss, CSS, HTML5, MongoDB, Mongoose, Cloudinary, Node Mailer, SendGrid, SWR"
+            " Next.js 13, React, Next-Auth, JavaScript, Tailwindcss, CSS, HTML5, MongoDB, Mongoose, Cloudinary, Node Mailer, SendGrid, SWR"
           }
           projectDescription={
             "Help adoption counselors improve pet adoption rates with impactful, fun, and tailor-fitted adoption profiles! Users can easily search and filter through community submitted names and descriptions. After signing up, users can save favorites, follow other users, and submit new names and descriptions."
